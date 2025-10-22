0b75f2be904536f7932df537e4ccbacdbf8d61be
Michał Jadach <michal.jadach@hoprnet.org>
2025-10-22 12:41:46 +0200

    better formatting of the checkMdStructure script

--- scripts/structure-check.sh
+++ scripts/structure-check.sh
@@ -31,12 +31,12 @@ printf '\n[Checking RFC headers - titles]\n'
 for file in "${md_files[@]}"; do
   first_line=$(head -n 1 "$file")
   if [[ $first_line =~ ^#\ RFC-[0-9]{4}:\ [A-Za-z0-9]{2,}.*$ ]]; then
-    echo "OK: $file"
+    echo " OK: $file"
   else
     # Print in red if missing or invalid
-    echo -e "\033[31mINVALID RFC HEADER: $file\033[0m"
-    echo -e "\033[31m Expected format:    # RFC-XXXX: Title\033[0m"
-    echo -e "\033[31m Found:              $first_line\033[0m"
+    echo -e "\033[31m INVALID RFC HEADER: $file\033[0m"
+    echo -e "\033[31m  Expected format:    # RFC-XXXX: Title\033[0m"
+    echo -e "\033[31m  Found:              $first_line\033[0m"
     counter=$((counter + 1))
   fi
 done
@@ -46,7 +46,7 @@ printf '\n[Checking RFC headers - space between title and the metadata]\n'
 for file in "${md_files[@]}"; do
   second_line=$(head -n 2 "$file" | tail -n 1)
   if [[ -z $second_line ]]; then
-    echo "OK: $file"
+    echo " OK: $file"
   else
     # Print in red if missing or invalid
     echo -e "\033[31mNO EMPTY LINE AFTER THE TITLE: $file\033[0m"
@@ -79,32 +79,32 @@ for file in "${md_files[@]}"; do
     # echo "RFC Title: $rfc_title"
   fi
 
-  echo -e "\nFile metadata: $file"
+  echo -e "\n File metadata: $file"
 
   # Check if the RFC Number in metadata matches the one in the first line
   if [[ "$rfc_number" != "- **RFC Number:** $first_line_rfc_number" ]]; then
-    echo -e "\033[31m RFC NUMBER MISMATCH\033[0m"
+    echo -e "\033[31m  RFC NUMBER MISMATCH\033[0m"
     valid=false
     counter=$((counter + 1))
   fi
 
   # Check if the RFC Title in metadata matches the one in the first line
   if [[ "$title" != "- **Title:** $first_line_rfc_title" ]]; then
-    echo -e "\033[31m RFC TITLE MISMATCH\033[0m"
+    echo -e "\033[31m  RFC TITLE MISMATCH\033[0m"
     valid=false
     counter=$((counter + 1))
   fi
 
   # Check if the RFC Status in metadata is valid using the generated regex
   if [[ ! "$status" =~ ^-\ \*\*Status:\*\*\ ($status_regex)$ ]]; then
-    echo -e "\033[31m RFC STATUS INVALID\033[0m"
+    echo -e "\033[31m  RFC STATUS INVALID\033[0m"
     valid=false
     counter=$((counter + 1))
   fi
 
   # Check if the RFC Authors in metadata is valid (any character after prefix)
   if [[ ! $authors =~ ^-\ \*\*Author\(s\):\*\*\ .+ ]]; then
-    echo -e "\033[31m RFC AUTHORS INVALID\033[0m"
+    echo -e "\033[31m  RFC AUTHORS INVALID\033[0m"
     valid=false
     counter=$((counter + 1))
   fi
@@ -113,9 +113,9 @@ for file in "${md_files[@]}"; do
   if [[ ! $created =~ ^-\ \*\*Created:\*\*\ [0-9]{4}-[0-9]{2}-[0-9]{2}$ ]]; then
 
     if [[ $created == "- **Created:** YYYY-MM-DD" ]]; then
-      echo -e "\033[31m RFC CREATED DATE NOT FILLED\033[0m"
+      echo -e "\033[31m  RFC CREATED DATE NOT FILLED\033[0m"
     else
-      echo -e "\033[31m RFC CREATED INVALID\033[0m"
+      echo -e "\033[31m  RFC CREATED INVALID\033[0m"
     fi
 
     valid=false
@@ -125,9 +125,9 @@ for file in "${md_files[@]}"; do
   # Check if the RFC Updated in metadata is valid (YYYY-MM-DD)
   if [[ ! $updated =~ ^-\ \*\*Updated:\*\*\ [0-9]{4}-[0-9]{2}-[0-9]{2}$ ]]; then
     if [[ $updated == "- **Updated:** YYYY-MM-DD" ]]; then
-      echo -e "\033[31m RFC UPDATED DATE NOT FILLED\033[0m"
+      echo -e "\033[31m  RFC UPDATED DATE NOT FILLED\033[0m"
     else
-      echo -e "\033[31m RFC UPDATED INVALID\033[0m"
+      echo -e "\033[31m  RFC UPDATED INVALID\033[0m"
     fi
     valid=false
     counter=$((counter + 1))
@@ -135,33 +135,33 @@ for file in "${md_files[@]}"; do
 
   # Check if the RFC Version in metadata is valid (vX.X.X, optional space, optional status)
   if [[ ! $version =~ ^-\ \*\*Version:\*\*\ v[0-9]+\.[0-9]+\.[0-9]+(\ \((($status_regex))\))?$ ]]; then
-    echo -e "\033[31m RFC VERSION INVALID\033[0m"
+    echo -e "\033[31m  RFC VERSION INVALID\033[0m"
     valid=false
     counter=$((counter + 1))
   fi
 
   # Check if the RFC Supersedes in metadata is valid (any character after prefix)
   if [[ ! $supersedes =~ ^-\ \*\*Supersedes:\*\*\ .+ ]]; then
-    echo -e "\033[31m RFC SUPERSEDES INVALID\033[0m"
+    echo -e "\033[31m  RFC SUPERSEDES INVALID\033[0m"
     valid=false
     counter=$((counter + 1))
   fi
 
   # Check if the RFC Related Links in metadata is valid (any character after prefix)
   if [[ ! $related_links =~ ^-\ \*\*Related\ Links:\*\*\ .+ ]]; then
-    echo -e "\033[31m RFC RELATED LINKS INVALID\033[0m"
+    echo -e "\033[31m  RFC RELATED LINKS INVALID\033[0m"
     valid=false
     counter=$((counter + 1))
   fi
 
   if [ "$valid" = true ]; then
-    echo " OK"
+    echo "  All OK"
   fi
 done
 
 if [ $counter -eq 0 ]; then
-  printf '\n[No issues found]\n'
+  printf '\033[38;5;28m\n[No issues found]\033[0m\n'
 else
-  printf '\n[Total issues found: %d]\n' "$counter"
+  printf "\033[31m\n[Total issues found: %d]\n\033[0m" "$counter"
   exit 1
 fi
